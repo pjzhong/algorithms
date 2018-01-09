9305bde06087feda1fdd52033d99151f53c9b0e2
pj_zhong <pj_zhong@163.com>
2018-01-09 20:22:01 +0800

    the one-bit

---
+++ src/test/java/Ones.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 1/7/2018.
+ * 
+ */
+public class Ones {
+
+    @Test
+    public void test() {
+        int ones = 0, n = 15;
+        System.out.println(Integer.bitCount(n));
+        for(int i = 32, num = n;i> 0; i--) {
+            if( (num & 1) == 1) { ones++; }
+            num = num >>> 1;
+        }
+        System.out.println(ones);
+        return;
+    }
+}
