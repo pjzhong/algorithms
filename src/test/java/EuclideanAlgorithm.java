1fc40ff8c96e4065cb22f9ed29bc57aff2c72d73
pj_zhong <pj_zhong@163.com>
2018-01-17 11:09:05 +0800

    add comment on EuclideanAlgorithm

--- src/test/java/EuclideanAlgorithm.java
+++ src/test/java/EuclideanAlgorithm.java
@@ -36,6 +36,17 @@ public class EuclideanAlgorithm {
         return  a < b ? gcdBySubtract(b - a, a) : gcdBySubtract(a - b, b);
     }
 
+    /*
+    * 两个正整数a和b（a>b），它们的最大公约数等于a-b的差值c和较小数b的最大公约数
+    *
+    * 当a和b均为偶数，gcb(a,b) = 2*gcb(a/2, b/2) = 2*gcb(a>>1, b>>1)
+    * 当a为偶数，b为奇数，gcb(a,b) = gcb(a/2, b) = gcb(a>>1, b)
+    * 当a为奇数，b为偶数，gcb(a,b) = gcb(a, b/2) = gcb(a, b>>1)
+    * 作者：程序员小灰
+    * 链接：https://juejin.im/post/5a217bac51882531926e8656
+    * 来源：掘金
+    * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    * */
     private long gcd(long a, long b) {
 
         if(a == b) {
