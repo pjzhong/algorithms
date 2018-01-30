fca5719570a0ddd26df97d5e2a49024f7be815a3
pj_zhong <pj_zhong@163.com>
2018-01-30 18:03:53 +0800

    add divisionTest

---
+++ src/test/java/DivisionTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2018/1/30.
+ */
+public class DivisionTest {
+
+    @Test
+    public void test() {
+        int[][] testCases = {
+                {12, 5},
+                {12, 3},
+                {12, 7},
+        };
+
+        for(int[] testCase : testCases) {
+            System.out.println(roundDown(testCase[0], testCase[1]));
+        }
+
+    }
+
+    public int roundDown(int dividend, int divisor) {
+        int c = dividend++, result = 0;
+
+        while(c > 0) {
+            c -= divisor;
+            result++;
+        }
+
+        return --result;
+    }
+}
