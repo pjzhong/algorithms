f9af5249a80446cb505a9c453eb277ab1f6027f7
pj_zhong <pj_zhong@163.com>
2018-03-17 16:08:11 +0800

    match brute-force implementation version-two

--- src/main/java/dsa/string/String.java
+++ src/main/java/dsa/string/String.java
@@ -22,20 +22,34 @@ public final class String {
         return null;
     }
 
+    /**
+     * 复杂度
+     *     最好情况(只经过一轮比较，即可确定匹配)：O(b.length)
+     *
+     *     最坏情况(每轮都比对至b的末字符， 且每轮如此)
+     *          每轮循环：比对 = b.length - 1(成功) + 1(失败) = bLength
+     *          循环次数 = a.length - b.length = 1
+     *          故 0(a.length * b.length)
+     *
+     *          最坏情况参考:
+     *              000000000000000.....001
+     *              000001
+     *
+     *      字符表越小， 最坏情况出现的概率越高
+     *      b.length越大， 最坏情况的后果更严重
+     * */
     private int match(char[] a, char[] b) {
-        int aLength = a.length, bLength = b.length;
-        int i = 0, j = 0;
-
-        while(i < aLength && j < bLength) {
-            if(a[i] == b[j]) {
-                ++i; ++j;
-            } else {
-                i = i - (j - 1);
-                j = 0;
+        int result = -1;
+        for(int i = 0, j = 0, bLength = b.length, aLength = a.length - bLength + 1; i < aLength; i++) {
+            for(j = 0; j < bLength; j++) {
+                if(a[i + j] != b[j]) {
+                    break;
+                }
             }
+            if(bLength <= j) { result = i; break; }
         }
 
-        return i - j;
+        return result;
     }
 
     public int indexOf(String s) {
