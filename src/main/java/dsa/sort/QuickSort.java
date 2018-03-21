80b2a3370bbd9ff395ed6abae822a0d0e095fbaf
pj_zhong <pj_zhong@163.com>
2018-03-21 18:36:44 +0800

    partition version-2 for quickSort

--- src/main/java/dsa/sort/QuickSort.java
+++ src/main/java/dsa/sort/QuickSort.java
@@ -12,29 +12,31 @@ public class QuickSort<E extends Comparable<E>> extends AbstractSort<E> {
     }
 
     private E[] quickSort(E[] e, int lo, int hi) {
-        if(hi - lo < 2) { return e; }
+        if(hi - lo < 1) { return e; }
         int mid = partition(e, lo, hi);
         quickSort(e, lo, mid);
         quickSort(e, mid + 1, hi);
         return e;
     }
 
-    private int partition(E[] e, int lo, int hi) {
-        swap(e, lo, lo + random.nextInt(hi - lo));
+    /***
+     * this implementation is more simpler than the previous
+     * in this version, @param e would  be separate into four parts
+     * e = [lo] + L(lo, mi] + G(mi, k) + U[u, hi]
+     * L < pivot <= G
+     * for more info, see the link below
+     * http://www.xuetangx.com/courses/course-v1:TsinghuaX+30240184_2X+sp/courseware/c15aad6e2dac4250934ea61d71deafd9/ab3cca35d795409f9693edd6cf25f192/
+     */
+    private int partition(E[] e, int lo, int hi) {//[lo, hi]
+        swap(e, lo, lo + random.nextInt(hi - lo + 1));
         E pivot = e[lo];
-        while(lo < hi) {
-            while((lo < hi) && lessEq(pivot, e[hi])) {
-                hi--;
+        int mi = lo;
+        for(int k = lo + 1; k <= hi; ++k) {
+            if(less(e[k], pivot)) {
+                swap(e, ++mi, k);
             }
-            swap(e, hi, lo);
-
-            while( (lo < hi) && lessEq(e[lo], pivot)) {
-                lo++;
-            }
-            swap(e, lo, hi);
         }
-
-        e[lo] = pivot;
-        return lo;
+        swap(e, lo, mi);
+        return mi;
     }
 }
