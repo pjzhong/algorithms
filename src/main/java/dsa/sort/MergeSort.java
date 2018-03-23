7dfe175ffaee988c6955297b0994ed007e1ac7ab
pj_zhong <pj_zhong@163.com>
2018-03-23 17:43:34 +0800

    little optimize

--- src/main/java/dsa/sort/MergeSort.java
+++ src/main/java/dsa/sort/MergeSort.java
@@ -1,52 +1,40 @@
 package dsa.sort;
 
-import java.lang.reflect.Array;
 
 public class MergeSort<E extends Comparable<E>> extends AbstractSort<E> {
 
     @Override
     public E[] sort(E[] e, int lo, int hi) {
-        E[] temp =(E[]) Array.newInstance(e.getClass().getComponentType(), e.length);
+        Object[] temp = new Object[e.length];
         MergeSort(e, lo, hi - 1, temp);
         return e;
     }
 
-    private int MergeSort(Comparable[] num, int begin, int end, Comparable[] temp) {
-        if(begin<end) {
-            int mid = (begin + end) / 2, inversion = 0;
-            inversion += MergeSort(num, begin, mid, temp);
-            inversion += MergeSort(num, mid+1, end, temp);
-            return inversion + Merge(num, begin, mid, end, temp);
+    private void MergeSort(Comparable[] num, int begin, int end, Object[] temp) {
+        if(begin < end) {
+            int mid = (begin + end) / 2;
+            MergeSort(num, begin, mid, temp);
+            MergeSort(num, mid+1, end, temp);
+            Merge(num, begin, mid, end, temp);
         }
-
-        return 0;
     }
 
-    private int Merge(Comparable[] num, int begin, int mid, int end, Comparable[] temp) {
+    private void Merge(Comparable[] num, int begin, int mid, int end, Object[] temp) {
         int left = begin,right = mid + 1;
         int start = 0;
 
-        int inversion = 0;
-        while(left <= mid && right <= end) {
-            if(less(num[left], num[right])) {
-                temp[start++] = num[left++];
-            } else {
-                temp[start++] = num[right++];
-                inversion += mid - left + 1;
-            }
+        for(;left <= mid && right <= end;start++) {
+            temp[start] = less(num[left], num[right]) ? num[left++] : num[right++];
         }
 
-        while(left<=mid) {//把left 到 mid 的元素复制到temp
-            temp[start++] = num[left++];
+        //把left 到 mid 的元素复制到temp
+        while(left <= mid) { temp[start++] = num[left++]; }
+
+        //把right 到end 的元素复制到temp
+        while(right <= end) { temp[start++] = num[right++]; }
+
+        for(int loop = 0; loop < start; loop++) {
+            num[begin + loop] = (Comparable) temp[loop];
         }
-
-        while(right<=end) {//把right 到end 的元素复制到temp
-            temp[start++] = num[right++];
-        }
-
-        for(int loop=0; loop < start; loop++)
-            num[begin+loop] = temp[loop];
-
-        return inversion;
     }
 }
