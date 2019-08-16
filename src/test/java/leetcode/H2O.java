e7e9503c00ffd07c10ddb4020140023c787f336e
pj_zhong <pj_zhong@163.com>
2019-08-16 17:39:47 +0800

    h20

---
+++ src/test/java/leetcode/H2O.java
@@ -0,0 +1,67 @@
+package leetcode;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class H2O {
+
+  private Semaphore H = new Semaphore(0);
+  private Semaphore O = new Semaphore(1);
+  private volatile AtomicInteger h = new AtomicInteger();
+
+  public H2O() {
+
+  }
+
+  public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+    H.acquire();
+    // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+    releaseHydrogen.run();
+    if (h.decrementAndGet() == 0) {
+      O.release();
+    }
+  }
+
+  public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+    O.acquire();
+    // releaseOxygen.run() outputs "H". Do not change or remove this line.
+    releaseOxygen.run();
+    h.set(2);
+    H.release(2);
+  }
+
+  public static void main(String[] args) {
+    H2O h2O = new H2O();
+
+    ExecutorService executorService = new ThreadPoolExecutor(0, Integer.MAX_VALUE,
+        0, TimeUnit.SECONDS,
+        new SynchronousQueue<>());
+
+    int o = 3;
+    for (int i = 0; i < o; i++) {
+      executorService.execute(() -> {
+        try {
+          h2O.oxygen(() -> System.out.print("O"));
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      });
+    }
+
+
+    for (int i = 0; i < o * 2; i++) {
+      executorService.execute(() -> {
+        try {
+          h2O.hydrogen(() -> System.out.print("H"));
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      });
+    }
+  }
+
+}
